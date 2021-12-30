1bd76714e53e71c56a5751ec58238725746ba808
lwolfish <58049823+lwolfish@users.noreply.github.com>
2021-12-30 18:12:54 -0500

    Created a blueprint for the methods in Part II

--- Finance/Calculator.cs
+++ Finance/Calculator.cs
@@ -109,5 +109,59 @@ namespace Finance
             return totalPnL;
         }
 
+        /*
+         * This method calculates the trading PnL for a given ticker on a given date
+         * @Hadassah, this method will require the Trade Quantity, Closing Price, and Trading Price for a specific ticker code on a given date
+         */
+        public double CalculateTradingPnL(String ticker, DateTime date)
+        {
+            double tradingPnL = 0;
+
+            //TODO
+
+            return tradingPnL;
+        }
+
+        /*
+         * This method will return the total quantity of a given ticker on a given date
+         * @Hadassah, this method will need the total quantity of the ticker before the trade
+         */
+        public double getTotalQuantity(String ticker, DateTime date)
+        {
+            double quantity = 0;
+
+            //TODO
+
+            return quantity;
+        }
+
+        /*
+         * This method calculates the positional PnL of a given ticker on a given date
+         * @Hadassah, this method will need the prior closing price of the ticker
+         */
+        public double CalculatePositionalPnL(String ticker, DateTime date)
+        {
+            double PnL = 0;
+
+            //TODO
+
+            return PnL;
+        }
+
+        /*
+         * This method returns the Total Daily PnL for a given ticker on a given date
+         */
+        public double CalculateTotalDailyPnL(String ticker, DateTime date)
+        {
+            return CalculateTradingPnL(ticker, date) + CalculatePositionalPnL(ticker, date);
+        }
+
+        /*
+         * This method returns the cumulative PnL for a given ticker on a given date
+         */
+        public double CalculateCumulativePnL(String ticker, DateTime date, double priorCumulativePnL)
+        {
+            return priorCumulativePnL + CalculateTotalDailyPnL(ticker, date);
+        }
     }
 }
