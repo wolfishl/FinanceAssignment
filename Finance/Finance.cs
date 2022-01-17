ab12d5df673b4117b8abf522622a25cdb50aec7e
wolfishl <78454249+wolfishl@users.noreply.github.com>
2022-01-17 11:38:41 -0500

    Revert "merging"

---
+++ Finance/Finance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finance
+{
+    public partial class Finance : Form
+    {
+        public Finance()
+        {
+            InitializeComponent();
+        }
+
+        private void Finance_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connect_click(object sender, EventArgs e)
+        {
+            Database database = new Database();
+            database.ConnectToDatabaseAndPullData(); 
+        }
+
+    }
+}
