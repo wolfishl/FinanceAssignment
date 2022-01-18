50fd81b984b7d7e08564904b3cca8479def87cdb
taliayahav <tyahav@student.touro.edu>
2022-01-17 19:47:48 -0500

    working on GUI

--- Finance/PnLReport.Designer.cs
+++ Finance/PnLReport.Designer.cs
@@ -28,12 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.portfolioValueLbl = new System.Windows.Forms.Label();
+            this.portfolioValueTxtBox = new System.Windows.Forms.TextBox();
+            this.rtuMoveLbl = new System.Windows.Forms.Label();
+            this.rtuMoveTxtBox = new System.Windows.Forms.TextBox();
+            this.predictedPnLLbl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.recalculateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Location = new System.Drawing.Point(12, 9);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(30, 13);
+            this.dateLabel.TabIndex = 0;
+            this.dateLabel.Text = "Date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 25);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(183, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // portfolioValueLbl
+            // 
+            this.portfolioValueLbl.AutoSize = true;
+            this.portfolioValueLbl.Location = new System.Drawing.Point(211, 9);
+            this.portfolioValueLbl.Name = "portfolioValueLbl";
+            this.portfolioValueLbl.Size = new System.Drawing.Size(78, 13);
+            this.portfolioValueLbl.TabIndex = 2;
+            this.portfolioValueLbl.Text = "Portfolio Value:";
+            // 
+            // portfolioValueTxtBox
+            // 
+            this.portfolioValueTxtBox.Location = new System.Drawing.Point(214, 25);
+            this.portfolioValueTxtBox.Name = "portfolioValueTxtBox";
+            this.portfolioValueTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.portfolioValueTxtBox.TabIndex = 3;
+            // 
+            // rtuMoveLbl
+            // 
+            this.rtuMoveLbl.AutoSize = true;
+            this.rtuMoveLbl.Location = new System.Drawing.Point(330, 9);
+            this.rtuMoveLbl.Name = "rtuMoveLbl";
+            this.rtuMoveLbl.Size = new System.Drawing.Size(63, 13);
+            this.rtuMoveLbl.TabIndex = 4;
+            this.rtuMoveLbl.Text = "RTU Move:";
+            // 
+            // rtuMoveTxtBox
+            // 
+            this.rtuMoveTxtBox.Location = new System.Drawing.Point(333, 25);
+            this.rtuMoveTxtBox.Name = "rtuMoveTxtBox";
+            this.rtuMoveTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.rtuMoveTxtBox.TabIndex = 5;
+            // 
+            // predictedPnLLbl
+            // 
+            this.predictedPnLLbl.AutoSize = true;
+            this.predictedPnLLbl.Location = new System.Drawing.Point(463, 9);
+            this.predictedPnLLbl.Name = "predictedPnLLbl";
+            this.predictedPnLLbl.Size = new System.Drawing.Size(77, 13);
+            this.predictedPnLLbl.TabIndex = 6;
+            this.predictedPnLLbl.Text = "Predicted PnL:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(466, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // recalculateButton
+            // 
+            this.recalculateButton.Location = new System.Drawing.Point(523, 201);
+            this.recalculateButton.Name = "recalculateButton";
+            this.recalculateButton.Size = new System.Drawing.Size(99, 23);
+            this.recalculateButton.TabIndex = 8;
+            this.recalculateButton.Text = "Recalculate";
+            this.recalculateButton.UseVisualStyleBackColor = true;
+            this.recalculateButton.Click += new System.EventHandler(this.recalculateButton_Click);
+            // 
+            // PnLReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(634, 236);
+            this.Controls.Add(this.recalculateButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.predictedPnLLbl);
+            this.Controls.Add(this.rtuMoveTxtBox);
+            this.Controls.Add(this.rtuMoveLbl);
+            this.Controls.Add(this.portfolioValueTxtBox);
+            this.Controls.Add(this.portfolioValueLbl);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dateLabel);
+            this.Name = "PnLReport";
             this.Text = "PnLReport";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label portfolioValueLbl;
+        private System.Windows.Forms.TextBox portfolioValueTxtBox;
+        private System.Windows.Forms.Label rtuMoveLbl;
+        private System.Windows.Forms.TextBox rtuMoveTxtBox;
+        private System.Windows.Forms.Label predictedPnLLbl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button recalculateButton;
     }
 }
--- Finance/PnLReport.cs
+++ Finance/PnLReport.cs
@@ -16,5 +16,23 @@ namespace Finance
         {
             InitializeComponent();
         }
+
+        private void recalculateButton_Click(object sender, EventArgs e)
+        {
+            recalculate();
+        }
+        void recalculate()
+        {
+            double value = recalculatePortfolioValue(dateTimePicker1.Value, rtuMoveTxtBox.Text);
+        }
+        double recalculatePortfolioValue(DateTime date, String changePercent)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
